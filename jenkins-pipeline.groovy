b46f77f958501fd9549c9e3276909cc8434330f3
Samuel Luciano <sa.lucianol@hotmail.com>
2018-12-06 19:21:51 -0400

    Initializing the pipeline for Jenkins.

---
+++ jenkins-pipeline.groovy
@@ -0,0 +1,30 @@
+pipeline{
+    agent none
+    stages{
+        stage("Initiating"){
+            steps{
+                sh(script: "echo Initiating", returnStdout: false)
+            }
+        }
+        stage("Building"){
+            steps{
+                sh(script: "echo Building", returnStdout: false)
+            }
+        }
+        stage("Deploying Dev"){
+            steps{
+                sh(script: "echo \"Deploying Dev\"")
+            }
+        }
+        stage("Testing Dev"){
+            steps{
+                sh(sript: "echo \"Testing Dev\"")
+            }
+        }
+        stage("Deploying QA"){
+            steps{
+                sh(script: "echo \"Deploying QA\"")
+            }
+        }
+    }
+}
