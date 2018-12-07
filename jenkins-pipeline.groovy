75428821bf3a72633b570cea976811e54ab3831e
Samuel Luciano <sa.lucianol@hotmail.com>
2018-12-06 20:02:14 -0400

    Remove the node and agent script.

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -1,33 +1,28 @@
 pipeline{
-    node{
-        agent {
-            label ""
+    stages{
+        stage("Initiating"){
+            steps{
+                sh(script: "echo Initiating", returnStdout: false)
+            }
         }
-        stages{
-            stage("Initiating"){
-                steps{
-                    sh(script: "echo Initiating", returnStdout: false)
-                }
+        stage("Building"){
+            steps{
+                sh(script: "echo Building", returnStdout: false)
             }
-            stage("Building"){
-                steps{
-                    sh(script: "echo Building", returnStdout: false)
-                }
+        }
+        stage("Deploying Dev"){
+            steps{
+                sh(script: "echo \"Deploying Dev\"")
             }
-            stage("Deploying Dev"){
-                steps{
-                    sh(script: "echo \"Deploying Dev\"")
-                }
+        }
+        stage("Testing Dev"){
+            steps{
+                sh(script: "echo \"Testing Dev\"")
             }
-            stage("Testing Dev"){
-                steps{
-                    sh(script: "echo \"Testing Dev\"")
-                }
-            }
-            stage("Deploying QA"){
-                steps{
-                    sh(script: "echo \"Deploying QA\"")
-                }
+        }
+        stage("Deploying QA"){
+            steps{
+                sh(script: "echo \"Deploying QA\"")
             }
         }
     }
